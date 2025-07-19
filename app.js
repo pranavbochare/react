1894debbd175d0062806814a011b609c4e5bcdf0
pranavbochare <pranavbochare15@gmail.com>
2025-07-19 12:36:21 +0530

    push

--- app.js
+++ app.js
@@ -14,18 +14,57 @@
         </div>
     </div>
 */
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "i am h1 tag"),
+//     React.createElement("h2", {}, "i am h2 tag"),
+//   ])
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(parent);
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i am h1 tag"),
-    React.createElement("h2", {}, "i am h2 tag"),
-  ])
-);
-
+// core react
+const heading = React.createElement("h1", { id: "heading" }, "Namaste react using react");
+console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
 
-root.render(parent);
+// jsx is not a html in js
+// it is html or xml like syntax
+// React Element
+const jsxHeading = <h1 id="heading"> Namaste React Using JSX</h1>;
+console.log(jsxHeading);
+//root.render(jsxHeading);
+
+// Functional Component use is rare
+const HeadingComponent = () => {
+  return <h1>Hello</h1>;
+};
+
+// functional component
+const HeadingComponent1 = () => <h1>performing component composition</h1>;
+
+const ele = <span>Adding Element to component</span>;
+const ele1 = <h1>Element to Element {ele}</h1>;
+// component composition
+const HeadingComponent2 = () => (
+  <div>
+    <HeadingComponent1 />
+    <HeadingComponent1> </HeadingComponent1>
+    {HeadingComponent1()}
+    {1234 + 655}
+    {ele1}
+    <h1 className="heading">Hello From Functional Component</h1>
+  </div>
+);
+root.render(<HeadingComponent2 />);
