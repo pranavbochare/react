4437aca8041484dfd4f6aceb255c5aadc70e67bf
pranavbochare <pranavbochare15@gmail.com>
2025-07-24 14:07:56 +0530

    navigation pages

--- src/components/Body.js
+++ src/components/Body.js
@@ -65,7 +65,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4.3);
-            setListOfRestaurants(filteredList);
+            setFilteredRestaurants(filteredList);
           }}>
           Top Rated Restaurants
         </button>
